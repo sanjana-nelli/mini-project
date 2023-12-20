709a833c942b8b59b11e148b08ff50df726b81bb
sanjana-nelli <93580121+sanjana-nelli@users.noreply.github.com>
2023-12-20 11:29:26 +0530

    app.js

--- static/app.js
+++ static/app.js
@@ -21,376 +21,52 @@ var data= {
     
     notes: {
         title:["Here you can find notes,<a href='https://vishnulearning.com/login/index.php'  target='_blank' >https://vishnulearning.com/login/index.php</a>"],
-        // options:["The rare green comet will make its closest pass of Earth between February 1 and February 2, according to NASA.","The rare green comet will make its closest pass of Earth between February 1 and February 2, according to NASA.","The rare green comet will make its closest pass of Earth between February 1 and February 2, according to NASA."],
-        // url : {
-        //     more:"https://www.youtube.com/@webhub/videos",
-        //     link:["https://www.youtube.com/@webhub/videos","https://www.youtube.com/@webhub/videos","https://www.youtube.com/@webhub/videos","https://www.youtube.com/@webhub/videos"]
-        // }
+        
     },
     papers: {
         title:["Here you can find previous year question papers <a href='https://www.manaresults.co.in/jntuh/jntuh-b-tech-old-previous-question-papers.php'  target='_blank' >https://www.manaresults.co.in/jntuh/jntuh-b-tech-old-previous-question-papers.php</a>"],
-        // options:['Electronics','Beauty products','Mobiles','Men Fashion','Women fashion'],
-        // url : {
-            
-        // }
+        
     },
-    electronics: {
-        title:["Thanks for your response","Here are some electronic items for you","Click on it to know more"],
-        options:['Televisions','Cameras','Gaming Consoles','Headphones','Speakers'],
-        url : {
-            more:"https://www.youtube.com/@webhub/videos",
-            link:["#","#","#","#","#"]
-        }
-    },
-    beauty: {
-        title:["Thanks for your response","Here are some beauty products for you","Click on it to know more"],
-        options:['Make-up & Nails','Skin Care','Fragrance','Hair care'],
-        url : {
-            more:"https://www.youtube.com/@webhub/videos",
-            link:["#","#","#","#"]
-        }
-    },
-    mobiles: {
-        title:["Thanks for your response","These are some results based on your input","Click on it to know more"],
-        options:['Mobile Phones','Cases & Covers','Power Banks','Tablets'],
-        url : {
-            more:"https://www.youtube.com/@webhub/videos",
-            link:["#","#","#","#"]
-        }
-    },
-    men: {
-        title:["Thanks for your response","These are some results based on your input","Click on it to know more"],
-        options:['Clothing','Shirts','T-shirts','Innerwear','Jeans'],
-        url : {
-            more:"https://www.youtube.com/@webhub/videos",
-            link:["#","#","#","#","#"]
-        }
-    },
-    women: {
-        title:["Thanks for your response","These are some results based on your input","Click on it to know more"],
-        options:['Clothing','Western Wear','Ethnic Wear','Top Brands'],
-        url : {
-            more:"https://www.youtube.com/@webhub/videos",
-            link:["#","#","#","#"]
-        }
-    },
-    music: {
-        title:["These are some latest songs <span class='emoji'> &#127925;</span>"],
-        options: ["song 1","song 2","song 3","song 4","song 5"],
-        url : {
-            more:"https://www.youtube.com/@webhub/videos",
-            link:["https://www.youtube.com/@webhub/videos","https://www.youtube.com/@webhub/videos","https://www.youtube.com/@webhub/videos","https://www.youtube.com/@webhub/videos"]
-        }
-    },
+    
+        
+    
     '2-1': {
         title: ["Here is 2-1 syllabus, In the 2nd year, 1st semester, our curriculum offers the subjects   Discrete Mathematics, Data Structures,Mathematical and Statistical Foundations,Computer Organization and Architecture,Python Programming,Business Economics & Financial Analysis,Data Structures Lab ,Python Programming Lab, Gender Sensitization Lab .<a href='https://jntuh.ac.in/uploads/academics/R18B.Tech.CSE(AIML)IIYearSyllabus.pdf' target='_blank' >https://jntuh.ac.in/uploads/academics/R18B.Tech.CSE(AIML)IIYearSyllabus.pdfp</a>"],
-        // options: ["Semester_Structure","Syllabus","Notes","Papers"],
-        // url: {
-        //     '2nd year':"https://jntuh.ac.in/uploads/academics/R18B.Tech.CSE(AIML)IIYearSyllabus.pdf",
-        //     link:["#","#","#","#","#"]
-        // }
+        
     },
     '2-2': {
         title: ["Here is 2-2 syllabus, the subjects included are  Formal Language and Automata Theory,Software Engineering,Operating Systems,Database Management Systems,Object Oriented Programming using Java,Operating Systems Lab,Database Management Systems Lab,Java Programming Lab,Constitution of India. <a href='https://jntuh.ac.in/uploads/academics/R18B.Tech.CSE(AIML)IIYearSyllabus.pdf' target='_blank' >https://jntuh.ac.in/uploads/academics/R18B.Tech.CSE(AIML)IIYearSyllabus.pdfp</a>"],
-        // options: ["Comedy","Horror","Sci-Fi","Romance","Action"],
-        // url: {
-        //     more:"https://www.youtube.com/@webhub/videos",
-        //     link:["#","#","#","#","#"]
-        // }
+        
     },
     '3-1': {
         title: ["Here is 3-1 syllabus,The subjects included are Design and Analysis of Algorithms, Machine Learning, Computer Networks, Compiler Design, Professional Elective - I, Professional Elective - II, Machine Learning Lab, Computer Networks Lab, Advanced Communication Skills Lab, Intellectual Property Rights. <a href='https://jntuh.ac.in/uploads/academics/R18B.Tech.CSE(AIML)IIIIVYearTentativeSyllabus.pdf' target='_blank' >https://jntuh.ac.in/uploads/academics/R18B.Tech.CSE(AIML)IIIIVYearTentativeSyllabus.pdf</a>"],
-        // options: ["Comedy","Horror","Sci-Fi","Romance","Action"],
-        // url: {
-        //     more:"https://www.youtube.com/@webhub/videos",
-        //     link:["#","#","#","#","#"]
-        // }
+        
     },
     '3-2': {
         title: ["Here is 3-2 syllabus,The subjects included are Artificial Intelligence, DevOps, Natural Language Processing, Professional Elective – III, Open Elective - I, Artificial Intelligence and Natural Language Processing Lab, DevOps Lab, Professional Elective - III Lab, Environmental Science.<a href='https://jntuh.ac.in/uploads/academics/R18B.Tech.CSE(AIML)IIIIVYearTentativeSyllabus.pdf' target='_blank' >https://jntuh.ac.in/uploads/academics/R18B.Tech.CSE(AIML)IIIIVYearTentativeSyllabus.pdf</a>"],
-        // url: {
-        //     more:"https://www.youtube.com/@webhub/videos",
-        //     link:["#","#","#","#","#"]
-        // }
+       
     },
     '4-1': {
         title: ["Here is 4-1 syllabus,The subjects included are Neural Networks & Deep Learning, Reinforcement Learning, Professional Elective - IV, Professional Elective - V, Open Elective - II, Deep Learning Lab, Industrial Oriented Mini Project/Summer Internship, Seminar, Project Stage - I.<a href='https://jntuh.ac.in/uploads/academics/R18B.Tech.CSE(AIML)IIIIVYearTentativeSyllabus.pdf' target='_blank' >https://jntuh.ac.in/uploads/academics/R18B.Tech.CSE(AIML)IIIIVYearTentativeSyllabus.pdf</a>"],
-        // url: {
-        //     more:"https://www.youtube.com/@webhub/videos",
-        //     link:["#","#","#","#","#"]
-        // }
+        
     },
     '4-2': {
         title: ["Here is 4-2 syllabus,The subjects included are Organizational Behaviour, Professional Elective - VI, Open Elective - III, Project Stage - II. <a href='https://jntuh.ac.in/uploads/academics/R18B.Tech.CSE(AIML)IIIIVYearTentativeSyllabus.pdf' target='_blank' >https://jntuh.ac.in/uploads/academics/R18B.Tech.CSE(AIML)IIIIVYearTentativeSyllabus.pdf</a>"],
-        // options: ["Comedy","Horror","Sci-Fi","Romance","Action"],
-        // url: {
-        //     more:"https://www.youtube.com/@webhub/videos",
-        //     link:["#","#","#","#","#"]
-        // }
+        
     },
     '2ndyear': {
         title: ["Here you can find 2nd year syllabus, <a href='https://jntuh.ac.in/uploads/academics/R18B.Tech.CSE(AIML)IIYearSyllabus.pdf' target='_blank' >https://jntuh.ac.in/uploads/academics/R18B.Tech.CSE(AIML)IIYearSyllabus.pdfp</a>"],
-        // options: ["Comedy", "Horror", "Sci-Fi", "Romance", "Action"],
-        // url: {
-        //     more: "https://www.youtube.com/@webhub/videos",
-        //     link: [
-        //         "https://www.youtube.com/@webhub/videos",
-        //         "https://www.youtube.com/@webhub/videos",
-        //         "https://www.youtube.com/@webhub/videos",
-        //         "https://www.youtube.com/@webhub/videos"
-        //     ]
-        // }
+        
     },
     '3rdyear': {
         title: ["Here you can find 3rd year syllabus, <a href='https://jntuh.ac.in/uploads/academics/R18B.Tech.CSE(AIML)IIIIVYearTentativeSyllabus.pdf' target='_blank' >https://jntuh.ac.in/uploads/academics/R18B.Tech.CSE(AIML)IIIIVYearTentativeSyllabus.pdf</a>"],
-        // options: ["Comedy", "Horror", "Sci-Fi", "Romance", "Action"],
-        // url: {
-        //     more: "https://www.youtube.com/@webhub/videos",
-        //     link: [
-        //         "https://www.youtube.com/@webhub/videos",
-        //         "https://www.youtube.com/@webhub/videos",
-        //         "https://www.youtube.com/@webhub/videos",
-        //         "https://www.youtube.com/@webhub/videos"
-        //     ]
-        // }
+        
     },
     '4thyear':{
         title: ["Here you can find 4th year syllabus, <a href='https://jntuh.ac.in/uploads/academics/R18B.Tech.CSE(AIML)IIIIVYearTentativeSyllabus.pdf' target='_blank' >https://jntuh.ac.in/uploads/academics/R18B.Tech.CSE(AIML)IIIIVYearTentativeSyllabus.pdf</a>"],
-        // options: ["Comedy", "Horror", "Sci-Fi", "Romance", "Action"],
-        // url: {
-        //     more: "https://www.youtube.com/@webhub/videos",
-        //     link: [
-        //         "https://www.youtube.com/@webhub/videos",
-        //         "https://www.youtube.com/@webhub/videos",
-        //         "https://www.youtube.com/@webhub/videos",
-        //         "https://www.youtube.com/@webhub/videos"
-        //     ]
-        // }
-    },
+    }
+
 }
-
-
-
-
-
-// var cbot= document.getElementById("chat-box");
-// var len1= data.chatinit.title.length;
-
-// document.getElementById('test').style.display='block';
-// initChat();
-
-
-// function initChat(){
-//     j=0;
-//     cbot.innerHTML='';
-//     for(var i=0;i<len1;i++){
-//         setTimeout(handleChat,(i*500));
-//     }
-//     setTimeout(function(){
-//         showOptions(data.chatinit.options)
-//     },((len1+1)*500))
-// }
-
-// var j=0;
-// function handleChat(){
-//     console.log(j);
-//     var elm= document.createElement("p");
-//     elm.innerHTML= data.chatinit.title[j];
-//     elm.setAttribute("class","msg");
-//     cbot.appendChild(elm);
-//     j++;
-//     handleScroll();
-// }
-
-// function showOptions(options){
-//     for(var i=0;i<options.length;i++){
-//         var opt= document.createElement("span");
-//         var inp= '<div>'+options[i]+'</div>';
-//         opt.innerHTML=inp;
-//         opt.setAttribute("class","opt");
-//         opt.addEventListener("click", handleOpt);
-//         cbot.appendChild(opt);
-//         handleScroll();
-//     }
-// }
-
-// function handleOpt(){
-//     console.log(this);
-//     var str= this.innerText;
-//     var textArr= str.split(" ");
-//     var findText= textArr[0];
-    
-//     document.querySelectorAll(".opt").forEach(el=>{
-//         el.remove();
-//     })
-//     var elm= document.createElement("p");
-//     elm.setAttribute("class","test");
-//     var sp= '<span class="rep">'+this.innerText+'</span>';
-//     elm.innerHTML= sp;
-//     cbot.appendChild(elm);
-
-//     console.log(findText.toLowerCase());
-//     var tempObj= data[findText.toLowerCase()];
-//     handleResults(tempObj.title,tempObj.options,tempObj.url);
-// }
-
-// function handleDelay(title){
-//     var elm= document.createElement("p");
-//         elm.innerHTML= title;
-//         elm.setAttribute("class","msg");
-//         cbot.appendChild(elm);
-// }
-
-
-// function handleResults(title,options,url){
-//     for(let i=0;i<title.length;i++){
-//         setTimeout(function(){
-//             handleDelay(title[i]);
-//         },i*500)
-        
-//     }
-
-//     const isObjectEmpty= (url)=>{
-//         return JSON.stringify(url)=== "{}";
-//     }
-
-//     if(isObjectEmpty(url)==true){
-//         console.log("having more options");
-//         setTimeout(function(){
-//             showOptions(options);
-//         },title.length*500)
-        
-//     }
-//     else{
-//         console.log("end result");
-//         setTimeout(function(){
-//             handleOptions(options,url);
-//         },title.length*500)
-        
-//     }
-// }
-// function handleResults(title, options, url) {
-//     for (let i = 0; i < title.length; i++) {
-//         setTimeout(function () {
-//             handleDelay(title[i]);
-//         }, i * 500);
-//     }
-
-//     const isObjectEmpty = (url) => {
-//         return !url || JSON.stringify(url) === "{}";
-//     };
-
-//     if (isObjectEmpty(url) === true) {
-//         console.log("having more options");
-//         setTimeout(function () {
-//             showOptions(options);
-//         }, title.length * 500);
-//     } else {
-//         console.log("end result");
-//         // Check if the response is for "2-1"
-//         if (title[0].toLowerCase().includes("2-1")) {
-//             setTimeout(function () {
-//                 handleTextResponse(title[0]);
-//             }, title.length * 500);
-//         } else {
-//             setTimeout(function () {
-//                 handleOptions(options, url);
-//             }, title.length * 500);
-//         }
-//     }
-// }
-
-// // // Add a new function to handle text response
-// // function handleTextResponse(text) {
-// //     var elm = document.createElement("p");
-// //     elm.innerHTML = text;
-// //     elm.setAttribute("class", "msg");
-// //     cbot.appendChild(elm);
-// //     handleScroll();
-// // }
-
-// // function handleResults(title, options, url) {
-// //     // Check if the response is for "2-1"
-// //     if (title[0].toLowerCase().includes("2-1")) {
-// //         handleTextResponse(title[0]);
-// //     } else {
-// //         for (let i = 0; i < title.length; i++) {
-// //             setTimeout(function () {
-// //                 handleDelay(title[i]);
-// //             }, i * 500);
-// //         }
-
-// //         const isObjectEmpty = (url) => {
-// //             return !url || JSON.stringify(url) === "{}";
-// //         };
-
-// //         if (isObjectEmpty(url) === true) {
-// //             console.log("having more options");
-// //             setTimeout(function () {
-// //                 showOptions(options);
-// //             }, title.length * 500);
-// //         } else {
-// //             console.log("end result");
-// //             setTimeout(function () {
-// //                 handleOptions(options, url);
-// //             }, title.length * 500);
-// //         }
-// //     }
-// // }
-
-// // Add a new function to handle text response
-// function handleTextResponse(text) {
-//     var elm = document.createElement("p");
-//     elm.innerHTML = text;
-//     elm.setAttribute("class", "msg");
-//     cbot.appendChild(elm);
-//     handleScroll();
-// }
-
-
-// function handleOptions(options,url){
-//     for(var i=0;i<options.length;i++){
-//         var opt= document.createElement("span");
-//         var inp= '<a class="m-link" href="'+url.link[i]+'">'+options[i]+'</a>';
-//         opt.innerHTML=inp;
-//         opt.setAttribute("class","opt");
-//         cbot.appendChild(opt);
-//     }
-//     var opt= document.createElement("span");
-//     var inp= '<a class="m-link" href="'+url.more+'">'+'See more</a>';
-
-//     const isObjectEmpty= (url)=>{
-//         return JSON.stringify(url)=== "{}";
-//     }
-
-//     console.log(isObjectEmpty(url));
-//     console.log(url);
-//     opt.innerHTML=inp;
-//     opt.setAttribute("class","opt link");
-//     cbot.appendChild(opt);
-//     handleScroll();
-// }
-
-// function handleScroll(){
-//     var elem= document.getElementById('chat-box');
-//     elem.scrollTop= elem.scrollHeight;
-// }
-// function handleKeyDown(event) {
-//     if (event.key === "Enter") {
-//         sendMessage();
-//     }
-// }
-
-
-// function handleTextResponse(text) {
-//     var elm = document.createElement("p");
-//     elm.innerHTML = text;
-//     elm.setAttribute("class", "msg");
-//     cbot.appendChild(elm);
-//     handleScroll();
-// }
-
-
 var cbot= document.getElementById("chat-box");
 var len1= data.chatinit.title.length;
 
